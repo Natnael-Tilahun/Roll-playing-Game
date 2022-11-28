93caaab03cdd9fdba51345e1a3c60225b34ebf15
natnaeltilahun <nataeltilahun@cbe.com.et>
2022-11-28 11:06:48 +0300

    Change image source url

--- data.js
+++ data.js
@@ -1,28 +1,28 @@
 const characterData = {
   hero: {
     name: "Wizard",
-    avatar: "public/wizard.png",
+    avatar: "/wizard.png",
     health: 60,
     diceCount: 3,
     currentDiceScore: [],
   },
   orc: {
     name: "Orc",
-    avatar: "public/orc.png",
+    avatar: "/orc.png",
     health: 30,
     diceCount: 1,
     currentDiceScore: [],
   },
   demon: {
     name: "Demon",
-    avatar: "public/demon.png",
+    avatar: "/demon.png",
     health: 25,
     diceCount: 2,
     currentDiceScore: [],
   },
   goblin: {
     name: "Goblin",
-    avatar: "public/goblin.png",
+    avatar: "/goblin.png",
     health: 20,
     diceCount: 3,
     currentDiceScore: [],
