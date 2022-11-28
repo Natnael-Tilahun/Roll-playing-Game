89d3e533c4c91e44c3ea46bf745de0339d5bd9bf
natnaeltilahun <nataeltilahun@cbe.com.et>
2022-11-28 10:58:38 +0300

    Change image source url

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import {defineConfig} from 'vite'
-
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: []	
-})
+  plugins: [
+    VitePWA({
+      mode: "development",
+      manifest: {
+        name: "App name",
+        short_name: "App short name",
+        description: "App description",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      },
+    }),
+  ],
+});
