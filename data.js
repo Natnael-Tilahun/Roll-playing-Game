d8d8074f321fa6c80e7011fe62d3be6e1fd4e11d
natnaeltilahun <nataeltilahun@cbe.com.et>
2022-11-28 11:04:25 +0300

    Change image source url

--- data.js
+++ data.js
@@ -1,28 +1,28 @@
 const characterData = {
   hero: {
     name: "Wizard",
-    avatar: "/images9.jpeg",
+    avatar: "public/wizard.png",
     health: 60,
     diceCount: 3,
     currentDiceScore: [],
   },
   orc: {
     name: "Orc",
-    avatar: "/orc.png",
+    avatar: "public/orc.png",
     health: 30,
     diceCount: 1,
     currentDiceScore: [],
   },
   demon: {
     name: "Demon",
-    avatar: "/demon.png",
+    avatar: "public/demon.png",
     health: 25,
     diceCount: 2,
     currentDiceScore: [],
   },
   goblin: {
     name: "Goblin",
-    avatar: "/goblin.png",
+    avatar: "public/goblin.png",
     health: 20,
     diceCount: 3,
     currentDiceScore: [],
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      mode: "development",
-      manifest: {
-        name: "App name",
-        short_name: "App short name",
-        description: "App description",
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
-      },
-    }),
-  ],
+  plugins: [],
 });
